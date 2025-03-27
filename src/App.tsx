a3c09c32d3dbe8891141258e14afab2b9e291b13
daugz <50736691+daugz@users.noreply.github.com>
2025-03-28 00:06:47 +0100

    feat: add fetch of inventory

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,29 @@
-import { useState } from 'react'
+import { useEffect, useState } from "react";
 
-import './App.css'
+import styles from "./App.module.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [inventory, setInventory] = useState([]);
 
+  const fetchInventory = async () => {
+    const inventoryResponse = await fetch("./inventory.json");
+    const inventoryData = await inventoryResponse.json();
+
+    setInventory(inventoryData?.items);
+  };
+
+  useEffect(() => {
+    try {
+      fetchInventory();
+    } catch (error) {
+      console.error("Error when fetching inventory", error);
+    }
+  }, []);
   return (
-    <>
-    </>
-  )
+    <div className={styles.body}>
+      <header className={styles.header}></header>
+    </div>
+  );
 }
 
-export default App
+export default App;
