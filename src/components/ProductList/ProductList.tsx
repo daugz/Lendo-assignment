ed2f2ced8b91ab4dcc7ab5d6aa0357a162661f43
daugz <50736691+daugz@users.noreply.github.com>
2025-03-28 17:31:05 +0100

    feat: display all options for products

--- src/components/ProductList/ProductList.tsx
+++ src/components/ProductList/ProductList.tsx
@@ -1,5 +1,5 @@
 import { type FC } from "react";
-import style from "./productList.module.css";
+import styles from "./productList.module.css";
 import { type Product } from "../../App";
 
 export const ProductList: FC<{ products: Product[] }> = ({ products }) => {
@@ -7,12 +7,49 @@ export const ProductList: FC<{ products: Product[] }> = ({ products }) => {
 
   return (
     products && (
-      <ol className={style.list}>
+      <ol className={styles.list}>
         {products.map((product: Product) => {
           return (
             <li key={product?.id}>
               <ul>
-                <li className={style.listItem}>{product?.name}</li>
+                <li className={styles.listItem}>
+                  <h2 className={styles.h2}>{product?.name}</h2>
+                </li>
+                <li className={styles.listItem}>
+                  <div className={styles.infoRow}>
+                    <p>{"Price:"}</p> <p>{product?.price} </p>
+                  </div>
+                </li>
+                <li className={styles.listItem}>
+                  <div className={styles.infoRow}>
+                    <p>{"Weight:"}</p> <p>{product?.weight} </p>
+                  </div>
+                </li>
+                <li className={styles.listItem}>
+                  <div className={styles.infoRow}>
+                    <p>{"Availability:"}</p> <p>{product?.available} </p>
+                  </div>
+                </li>
+                <li className={styles.listItem}>
+                  <div className={styles.infoRow}>
+                    <p>{"Brand:"}</p> <p>{product?.brand} </p>
+                  </div>
+                </li>
+                <li className={styles.listItem}>
+                  <div className={styles.infoRow}>
+                    {"options"}
+                    {product?.options.map((option) => {
+                      return (
+                        <div className={""}>
+                          <p>{option.color}</p>
+                          <p>{option.power}</p>
+                          <p>{option.quantity}</p>
+                          <p>{option.storage}</p>
+                        </div>
+                      );
+                    })}
+                  </div>
+                </li>
               </ul>
             </li>
           );
