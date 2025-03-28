b9cd177540ea4050f71b2d54635422d3f9682c55
daugz <50736691+daugz@users.noreply.github.com>
2025-03-28 18:18:31 +0100

    feat: add Card component into Products List

--- src/components/ProductList/ProductList.tsx
+++ src/components/ProductList/ProductList.tsx
@@ -1,58 +1,16 @@
 import { type FC } from "react";
 import styles from "./productList.module.css";
 import { type Product } from "../../App";
+import { Card } from "../Card/Card";
 
 export const ProductList: FC<{ products: Product[] }> = ({ products }) => {
   if (products.length <= 0) return null;
 
   return (
     products && (
-      <ol className={styles.list}>
+      <ol className={styles.orderedList}>
         {products.map((product: Product) => {
-          return (
-            <li key={product?.id}>
-              <ul>
-                <li className={styles.listItem}>
-                  <h2 className={styles.h2}>{product?.name}</h2>
-                </li>
-                <li className={styles.listItem}>
-                  <div className={styles.infoRow}>
-                    <p>{"Price:"}</p> <p>{product?.price} </p>
-                  </div>
-                </li>
-                <li className={styles.listItem}>
-                  <div className={styles.infoRow}>
-                    <p>{"Weight:"}</p> <p>{product?.weight} </p>
-                  </div>
-                </li>
-                <li className={styles.listItem}>
-                  <div className={styles.infoRow}>
-                    <p>{"Availability:"}</p> <p>{product?.available} </p>
-                  </div>
-                </li>
-                <li className={styles.listItem}>
-                  <div className={styles.infoRow}>
-                    <p>{"Brand:"}</p> <p>{product?.brand} </p>
-                  </div>
-                </li>
-                <li className={styles.listItem}>
-                  <div className={styles.infoRow}>
-                    {"options"}
-                    {product?.options.map((option) => {
-                      return (
-                        <div className={""}>
-                          <p>{option.color}</p>
-                          <p>{option.power}</p>
-                          <p>{option.quantity}</p>
-                          <p>{option.storage}</p>
-                        </div>
-                      );
-                    })}
-                  </div>
-                </li>
-              </ul>
-            </li>
-          );
+          return <Card product={product} />;
         })}
       </ol>
     )
