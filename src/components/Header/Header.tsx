b0277d3567a00686e0c99a4b900eb54eee8774d4
daugz <50736691+daugz@users.noreply.github.com>
2025-03-29 20:33:35 +0100

    feat: add navigation

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -9,6 +9,9 @@ export const Header: FC<Header> = ({ cartCount }) => {
   return (
     <header className={styles.header}>
       <div className={styles.headerContainer}>
+        <a href="tel:0771131310" className={styles.telephone}>
+          telephone
+        </a>
         <a href="/" className={styles.headingLink}>
           <img src={"/Lendo-image.svg"} />
         </a>
@@ -17,6 +20,14 @@ export const Header: FC<Header> = ({ cartCount }) => {
           <div className={styles.shoppingCartImage}></div>
         </a>
       </div>
+      <nav className={styles.navLinkContainer}>
+        <a href="/" className={styles.navLink}>
+          Home
+        </a>
+        <a className={styles.navLink} href={"/checkout"}>
+          Checkout
+        </a>
+      </nav>
     </header>
   );
 };
