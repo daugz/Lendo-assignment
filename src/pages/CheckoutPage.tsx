f6fb85bd568ac267e92ea58355939bfffc6657de
daugz <50736691+daugz@users.noreply.github.com>
2025-03-30 12:38:13 +0200

    feat: add checkout page with checkout cards

--- src/pages/CheckoutPage.tsx
+++ src/pages/CheckoutPage.tsx
@@ -1,5 +1,61 @@
-import React from "react";
+import React, { FC } from "react";
+import styles from "./checkoutPage.module.css";
+import { Product } from "../types";
 
 export const CheckoutPage = () => {
-  return <div>CheckoutPage</div>;
+  const getAllCheckoutItems = () => {
+    let cart: Product[] = [];
+    if (sessionStorage?.length) {
+      for (let index = 0; index < sessionStorage?.length; index++) {
+        const key = sessionStorage.key(index);
+        if (key) {
+          const product = sessionStorage.getItem(key);
+          if (product) {
+            cart = [...cart, JSON.parse(product)];
+          }
+        }
+      }
+    }
+    return cart;
+  };
+  const checkoutItems = getAllCheckoutItems();
+
+  const handleOnClick = () => {
+    const isCartAlreadyEmpty = sessionStorage?.length === 0;
+
+    if (!sessionStorage?.length || !isCartAlreadyEmpty) {
+      sessionStorage.clear();
+    }
+  };
+  return (
+    <div className={styles.container}>
+      <h1 className={styles.h1}>Checkout</h1>
+
+      <div className={styles.checkoutCardContainer}>
+        {checkoutItems.map((item) => {
+          return <CheckoutCard item={item} />;
+        })}
+      </div>
+      <button onClick={handleOnClick}>Remove all items in cart</button>
+    </div>
+  );
+};
+
+const CheckoutCard: FC<{ item: any }> = ({ item }) => {
+  return (
+    <div className={styles.checkOutCard}>
+      <div className={styles.imageContainer}>
+        <img className={styles.image} src={item?.image} />
+      </div>
+      <div className={styles.infoContainer}>
+        {item.brand && <div>{item.brand}</div>}
+        <div>{item.color}</div>
+        {item.name && <div>{item.name}</div>}
+        {item.power && <div>{item.power}</div>}
+        {item.price && <div>{item.price}</div>}
+        {item.storage && <div>{item.storage}</div>}
+        {item.weight && <div>{item.weight}</div>}
+      </div>
+    </div>
+  );
 };
