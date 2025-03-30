52aa9bf9541c7232f2c09ed7e3d4ca211d180717
daugz <50736691+daugz@users.noreply.github.com>
2025-03-30 21:48:42 +0200

    feat: add message for added product

--- src/pages/detailsPage/DetailsPage.tsx
+++ src/pages/detailsPage/DetailsPage.tsx
@@ -70,6 +70,10 @@ export const DetailsPage = ({
       console.log("Added to cart");
       sessionStorage.setItem(cartProductId, JSON.stringify(chosenProduct));
       setShoppingCart([...shoppingCart, cartProductId]);
+      return {
+        type: "added",
+        message: `Product successfully added to cart`,
+      };
     }
     if (quantity === 0)
       return {
@@ -107,7 +111,7 @@ export const DetailsPage = ({
           <input type="text" hidden name="name" value={productDetails?.name} />
           <h2 className={styles.h2}>{productDetails?.name}</h2>
           {<Available Available={productDetails?.available} />}
-          <div className={styles.productInfoContainer}>
+          <div className={styles.productInfoPrice}>
             {productDetails?.price} kr{" "}
             <input hidden name="price" value={productDetails?.price} />
           </div>
@@ -123,9 +127,31 @@ export const DetailsPage = ({
             {productDetails?.brand}
           </div>
           {productDetails?.options?.length > 0 && (
-            <ProductOptions options={productDetails.options} />
+            <ProductOptions
+              options={productDetails.options}
+              available={productDetails.available}
+            />
           )}
           <div>
+            {state?.type === "added" && (
+              <div className={styles.messagePopUp}>
+                <div className={styles.checkMark}>
+                  <svg
+                    viewBox="0 0 128 128"
+                    width="40px"
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="green"
+                  >
+                    <g>
+                      <g>
+                        <path d="M85.263,46.49L54.485,77.267L42.804,65.584c-0.781-0.782-2.047-0.782-2.828-0.002c-0.781,0.782-0.781,2.048,0,2.829    l14.51,14.513l33.605-33.607c0.781-0.779,0.781-2.046,0-2.827C87.31,45.708,86.044,45.708,85.263,46.49z M64.032,13.871    c-27.642,0-50.129,22.488-50.129,50.126c0.002,27.642,22.49,50.131,50.131,50.131h0.004c27.638,0,50.123-22.489,50.123-50.131    C114.161,36.358,91.674,13.871,64.032,13.871z M64.038,110.128h-0.004c-25.435,0-46.129-20.694-46.131-46.131    c0-25.434,20.693-46.126,46.129-46.126s46.129,20.693,46.129,46.126C110.161,89.434,89.471,110.128,64.038,110.128z" />
+                      </g>
+                    </g>
+                  </svg>
+                </div>{" "}
+                <p>{state?.message}</p>
+              </div>
+            )}
             <AddToCartButton available={productDetails?.available} />
           </div>
         </div>
@@ -143,7 +169,10 @@ const AddToCartButton: FC<{ available: boolean }> = ({ available }) => {
   );
 };
 
-const ProductOptions: FC<{ options: Option[] }> = ({ options }) => {
+const ProductOptions: FC<{ options: Option[]; available: boolean }> = ({
+  options,
+  available,
+}) => {
   const [optionsDisplayed, setOptionsDisplayed] = useState<Option>(options[0]);
   const [colorSelected, setColorSelected] = useState(
     Array.isArray(optionsDisplayed.color)
@@ -187,7 +216,9 @@ const ProductOptions: FC<{ options: Option[] }> = ({ options }) => {
           );
         })}
       </div>
-      {optionsDisplayed?.quantity && optionsDisplayed?.quantity !== 0 ? (
+      {optionsDisplayed?.quantity &&
+      optionsDisplayed?.quantity !== 0 &&
+      available ? (
         <>
           <input hidden name="quantity" value={optionsDisplayed.quantity} />
           <div>Quantity: {optionsDisplayed.quantity}</div>
