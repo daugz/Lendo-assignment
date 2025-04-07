0a010650e90ec634a459c552ee0c678a9bdf5b85
daugz <50736691+daugz@users.noreply.github.com>
2025-04-07 18:39:11 +0200

    chore: make findColor more dynamic

--- src/utils/findColor.ts
+++ src/utils/findColor.ts
@@ -1,10 +1,5 @@
 import styles from "./findColor.module.css";
 
 export const findColor = (color: string): CSSModuleClasses[string] | null => {
-  if (color === "red") return styles.red;
-  if (color === "white") return styles.white;
-  if (color === "green") return styles.green;
-  if (color === "black") return styles.black;
-  if (color === "orange") return styles.orange;
-  else return null;
+  return styles[color];
 };
